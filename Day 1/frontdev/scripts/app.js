dd92dbf567a940b8e88acb9da0662bdf302ec05d
adelahintiu <adela.hintiu@gmail.com>
2016-06-25 16:45:44 +0300

    FrontendCode1 my commit

--- Day 1/frontdev/scripts/app.js
+++ Day 1/frontdev/scripts/app.js
@@ -32,7 +32,7 @@ var employeesList = [
 ];
 
 function showList(){
-    var myTable = '<table border="1" class="table table-bordered"> <tr><th>First Name</th>' +
+    var myTable = '<table border="1" id="table" class="table table-bordered"> <tr><th>First Name</th>' +
         '<th>Last Name</th><th>Phone</th><th>Salary</th><th></th><th></th></tr>';
 
     for(var i in employeesList){
@@ -137,6 +137,7 @@ function viewDetails(btn) {
 function deleteEmployee(btn){
     var row = btn.parentNode.parentNode;
     row.parentNode.removeChild(row);
+    employeesList.splice(row, 1);
     
 }
 
@@ -168,6 +169,125 @@ function deleteLast() {
 function someFunctions() {
     addEmployee();
     showList();
+}
+
+function sortFunctions(){
+    var option = document.getElementById('sort').value;
+    
+    if(option == 1){
+        /* function to sort alphabetically by column */
+        employeesList.sort(function(a,b){
+          if(a.firstName < b.firstName){
+            return -1;    
+          }
+          if(b.firstName < a.firstName){
+              return 1;
+          }  
+          return 0;
+        })
+        showList();
+        
+    }
+    else if(option == 2){
+        employeesList.sort(function(a,b){
+            if(a.lastName < b.lastName){
+                return -1;
+            }
+            if(b.lastName < a.lastName){
+                return 1;
+            }
+            return 0;
+        })
+        showList();
+    }
+    else if(option == 3){
+        employeesList.sort(function(a,b){
+            if(a.phone < b.phone){
+                return -1;
+            }
+            if(b.phone < a.phone){
+                return 1;
+            }
+            return 0;
+        })
+        showList();
+    }
+    else if(option == 4){
+        employeesList.sort(function(a,b){
+            if(a.salary < b.salary){
+                return -1;
+            }
+            if(b.salary < a.salary){
+                return 1;
+            }
+            return 0;
+        })
+        showList();
+    }
+    
+}
+function findWord() {
+    var input = document.getElementById('find').value;
+    var missingFirstN = 0;
+    var missingLastN = 0;
+    var missingPhone = 0;
+    var missingSalary = 0;
+    var poz1 = -1;
+    var poz2 = -1;
+    var poz3 = -1;
+    var poz4 = -1;
+    for(var i = 0; i < employeesList.length; i++){
+        var elem1 = employeesList[i].lastName;
+        var elem2 = employeesList[i].firstName;
+        var elem3 = employeesList[i].phone;
+        var elem4 = employeesList[i].salary;
+        if(input == elem1){
+            missingLastN = elem1;
+            poz1 = i;
+        }
+        else if(input == elem2){
+            missingFirstN = elem2;
+            poz2 = i;
+        }
+        else if(input == elem3){
+            missingPhone = elem3;
+            poz3 = i;
+        }
+        else if(input == elem4){
+            missingSalary = elem4;
+            poz4 = i;
+        }
+        
+        
+    }
+
+
+    /* Table manipulation method */
+    if(missingFirstN != 0 || missingLastN != 0 || missingPhone != 0 || missingSalary != 0){
+        var myTable = '<table border="1" id="table" class="table table-bordered"> <tr><th>First Name</th>' +
+            '<th>Last Name</th><th>Phone</th><th>Salary</th><th></th><th></th></tr>';
+
+        for(var i = 0; i < employeesList.length; i++){
+            if(i == poz1 || i == poz2 || i == poz3 || i == poz4) {
+                myTable += '<tr><td>' + employeesList[i].firstName + '</td><td>' + employeesList[i].lastName + '</td><td>'
+                    + employeesList[i].phone + '</td><td>' + employeesList[i].salary + ' </td><td>' + '<input type="button" value="Vizualizare" onclick="viewDetails(this)"/>'
+                    + ' </td><td>' + '<input type="button" value="Sterge" onclick="deleteEmployee(this)"/>' + '</td></tr>';
+            }    
+
+        }
+        myTable += '<tr id="line"><td>'+mostFreq()+'</td><td>'+countUnique()+'</td><td>'+phoneFreq()+'</td><td>'+avgSalary()+'</td></tr>';
+        myTable += '</table>';
+
+        var container = document.getElementById('listcontainer');
+        container.innerHTML = myTable;
+    }
+    else{
+        window.alert("Employee can't be found!")
+    }
+        
+    
+    
+    
 }
 
 function totalSalary() {
